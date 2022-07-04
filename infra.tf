266e31376a5db323b90c50b9f81399bfb02b7978
zineb-ch <72198154+zineb-ch@users.noreply.github.com>
2022-07-04 03:27:37 +0200

    Update infra.tf

--- infra.tf
+++ infra.tf
@@ -98,28 +98,6 @@ resource "aws_subnet" "subnet-private-3" {
   }
 }
 
-/*
-## SG Rule egress
-resource "aws_security_group_rule" "web_egress_allow_all" {
-  type              = "egress"
-  to_port           = 0
-  protocol          = "-1"
-  from_port         = 0
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.allow_nat.id
-}
-
-## SG Rule ingress
-resource "aws_security_group_rule" "ingress_allow_private" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = -1
-  cidr_blocks       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  security_group_id = aws_security_group.allow_nat.id
-}
-*/
-
 # Route Table
 ## Private
 ### Use Main Route Table
