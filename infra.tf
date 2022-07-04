997755afee7935ee94b613c087f689b485c5ee39
zineb-ch <72198154+zineb-ch@users.noreply.github.com>
2022-07-04 03:31:14 +0200

    Update infra.tf

--- infra.tf
+++ infra.tf
@@ -97,18 +97,59 @@ resource "aws_subnet" "subnet-private-3" {
     Name = "${var.env}-subnet-private-3"
   }
 }
-
+/*
+# Nat Instance
+resource "aws_instance" "nat" {
+  ami                    = "ami-0f630a3f40b1eb0b8"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.subnet-public-1.id
+  vpc_security_group_ids = [aws_security_group.allow_nat.id]
+  source_dest_check      = "false"
+  user_data = <<-EOF
+        #!/bin/bash
+        sysctl -w net.ipv4.ip_forward=1 /sbin/
+        iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+  EOF
+  tags = {
+    Name = "${var.env}-NatInstance"
+  }
+}
+# Nat SG
+resource "aws_security_group" "allow_nat" {
+  name        = "allow_web"
+  vpc_id      = aws_vpc.vpc.id
+  description = "Allow inbound traffic"
+}
+## SG Rule egress
+resource "aws_security_group_rule" "web_egress_allow_all" {
+  type              = "egress"
+  to_port           = 0
+  protocol          = "-1"
+  from_port         = 0
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.allow_nat.id
+}
+## SG Rule ingress
+resource "aws_security_group_rule" "ingress_allow_private" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = -1
+  cidr_blocks       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  security_group_id = aws_security_group.allow_nat.id
+}
+*/
 # Route Table
 ## Private
 ### Use Main Route Table
 resource "aws_default_route_table" "main-private" {
   default_route_table_id = aws_vpc.vpc.default_route_table_id
-
+/*
   route {
     cidr_block  = "0.0.0.0/0"
     instance_id = aws_instance.nat.id
   }
-
+*/
   tags = {
     Name = "${var.env}-rt-main-private"
   }
@@ -142,4 +183,8 @@ resource "aws_route_table_association" "public-2" {
 resource "aws_route_table_association" "public-3" {
   subnet_id      = aws_subnet.subnet-public-3.id
   route_table_id = aws_route_table.public.id
+}
+terraform {
+  backend "s3"{
+  }
 }
